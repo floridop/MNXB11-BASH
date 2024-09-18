cc00e6fab09606d9a87c3b4e5ae503335bdd6ce8
Florido Paganelli <florido.paganelli@gmail.com>
2024-09-18 22:48:40 +0200

    Created new exercise 4.7

--- exercises/ex4.7/answers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# set the variable to the first input parameter
-# to the answers.sh script
-ANSWER=YOUR_CODE_HERE
-
-# print the content of ANSWER to screen
-echo "You asked me to print: $ANSWER"
-
--- exercises/ex4.7/solution/answers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# set the variable to the first input parameter
-# to the answers.sh script
-ANSWER=$1
-
-# print the content of ANSWER to screen
-echo "You asked me to print: $ANSWER"
-
