899012b2709905594b0ed22bd08d39dec59920b6
Florido Paganelli <florido.paganelli@hep.lu.se>
2025-09-16 13:37:51 +0200

    Added better text and the $@ variable to predefinedvars.sh

--- examples/predefinedvars.sh
+++ examples/predefinedvars.sh
@@ -6,7 +6,8 @@
 # print out info about arguments to this script
 echo "Number of arguments: $#"
 echo "Name of this script: $0"
-echo "Arguments: $1 $2 $3 $4"
+echo "First 4 arguments: $1 $2 $3 $4"
+echo "Arguments as a single string: $@"
 
 # print this script's Process IDentifier:
 echo "PID is $$"
