2a3b8dd8ad86caf5138a833b0e0e9ef891d7a5a5
Florido Paganelli <florido.paganelli@hep.lu.se>
2025-09-17 15:37:35 +0200

    added ex09 test exit status

---
+++ exercises/ex09/README.md
@@ -0,0 +1,8 @@
+Test the `exit` command and its parameters
+
+1) Comment out only one of the `exit` lines in the `answers.sh` file 
+2) Run the script
+3) Test that the output value matches the one in the `exit` statement with
+`echo $?`
+
+Repeat for all the three commands
---
+++ exercises/ex09/solution/answers_exit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define and initialize a variable called ANSWERS with the number 42
+ANSWERS=42
+
+# Print to screen the content of the variable
+echo $ANSWERS
+
+echo "Test the exit status of this file by typing"
+echo '  echo $?'
+echo
+
+# Uncomment only one of the exit lines below on each run
+
+# exit with no parameters is equivalent to exit 0
+exit
+
+# exit with no error 
+#exit 0
+
+# exit with error
+#exit 1
+
+# commenting all the exit lines is equivalent to exiting with no error
+
+
