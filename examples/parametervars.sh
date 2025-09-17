411356d7820b97602aa1ad1b51107b5f4c17c359
Florido Paganelli <florido.paganelli@hep.lu.se>
2025-09-17 15:45:16 +0200

    Changed predefinedvars.sh to parametervars.sh, only showcasing parameter variables

--- examples/parametervars.sh
+++ examples/parametervars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# predefinedvars.sh
-# call with: ./predefinedvars.sh arg1 arg2 arg3
+# parametervars.sh
+# call with: ./parametervars.sh arg1 arg2 arg3
 #
 
 # print out info about arguments to this script
@@ -9,5 +9,3 @@ echo "Name of this script: $0"
 echo "First 4 arguments: $1 $2 $3 $4"
 echo "Arguments as a single string: $@"
 
-# print this script's Process IDentifier:
-echo "PID is $$"
