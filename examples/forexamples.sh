31fab3696d9d6a3c6288a19f58da3af8d0f233fc
Florido Paganelli <florido.paganelli@hep.lu.se>
2025-09-17 17:40:18 +0200

    adding C style for syntax to forexamples.sh

--- examples/forexamples.sh
+++ examples/forexamples.sh
@@ -20,3 +20,13 @@ echo "| __ ) / _ \ / _ \|  \/  | |"
 echo "|  _ \| | | | | | | |\/| | |"
 echo "| |_) | |_| | |_| | |  | |_|"
 echo "|____/ \___/ \___/|_|  |_(_)"
+echo
+
+# Same as above but with a slightly different syntax
+for ((i=10; i>0; i--))
+do
+  echo "now counting $i with a different syntax"
+  # wait 1 second
+  sleep 1
+done
+echo " bim! "
