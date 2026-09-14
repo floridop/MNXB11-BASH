2cf4c77d84279fda28fbda9ff4b1b02ddcb35b35
Florido Paganelli <florido.paganelli@fysik.lu.se>
2026-09-14 17:03:06 +0200

    Added comments to the container script to run compilation

--- examples/container/calculatePI/scripts/build_in_container_calculatePI.sh
+++ examples/container/calculatePI/scripts/build_in_container_calculatePI.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# This wrapper script launches the course container and builds calculatePI in it
+# This script launches the course container and builds calculatePI inside it
 
-# These commands below run the wrapper scripts using the container `mnxb11_al9.latest.sif` with minimum required parameters
+# These commands below run the build script using the container `mnxb11_al9.latest.sif` with minimum required parameters
 # -B mounts the project directory inside the container so that is visible also inside the container
-# You can add parameters to the application by adding them at the end of the line
-# Example: apptainer run -B /projects:/projects /projects/hep/fs10/mnxb11/containers/mnxb11_al9.latest.sif ./buildnrun_picalc.sh 1e9
+# One can add parameters to the application build script by adding them at the end of the line
+# Example: apptainer run -B /projects:/projects /projects/hep/fs11/share/mnxb11/containers/mnxb11_al9.latest.sif ./calculatePI_build.sh 1e9 path-to-outputfile path-to-sources
 
-# Passing the parameter '3e8' here to increase job duration. Can be changed
+# Passing the parameter '3e8' here to increase job duration.
+# /tmp/ will contain the generated binary file, see output messages at the end of the compilation
 apptainer run -B /projects:/projects /projects/hep/fs11/share/mnxb11/containers/mnxb11_al9.latest.sif ./calculatePI_build.sh 3e8 /tmp/ 
 
