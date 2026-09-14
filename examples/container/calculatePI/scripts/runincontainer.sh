f55f47b15cf059b467a379359de59fe25d17ba26
Florido Paganelli <florido.paganelli@fysik.lu.se>
2026-09-14 09:50:02 +0200

    Changed runincontaner.sh script to be POSIX compliant

--- examples/container/calculatePI/scripts/runincontainer.sh
+++ examples/container/calculatePI/scripts/runincontainer.sh
@@ -1,39 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
-# TODO: add options to compile or run or both
-DOBUILD=${1:-'no'}
-EXECUTE=${1:-'no'}
+# By default don't do anything
+DOBUILD=''
+EXECUTE=''
 
 for param in $@; do 
-  if [[ $param == '--build' ]]; then
+  if [ $param == '--build' ]; then
      DOBUILD='yes'
   fi
-  if [[ $param == '--run' ]]; then
+  if [ $param == '--run' ]; then
      EXECUTE='yes'
   fi
 done
 
-# TODO: change text below
-# This command below runs the script using the container `mnxb11_al9.latest.sif` with minimum required parameters
-# It is simpler than startmnxb11container.sh because we need much less options to run in batch
-# It is best for running on batch systems
+# If both vars are empty (no parameters), show error message and exit with error
+if [ "$#" -eq 0 ]; then
+  echo "Please specify one of the options"
+  echo "  --build for building"
+  echo "  --run for execution"
+  exit 1
+fi
+
+# These commands below run the wrapper scripts using the container `mnxb11_al9.latest.sif` with minimum required parameters
 # -B mounts the project directory inside the container so that is visible also inside the container
 # You can add parameters to the application by adding them at the end of the line
 # Example: apptainer run -B /projects:/projects /projects/hep/fs10/mnxb11/containers/mnxb11_al9.latest.sif ./buildnrun_picalc.sh 1e9
 
-if [[ "x$DOBUILD" == 'xyes' ]]; then
-   # Passing a parameter to increase job duration
+# Launch build code wrapper
+if [ "x$DOBUILD" == 'xyes' ]; then
+   # Passing a parameter here to increase job duration. Can be changed
    apptainer run -B /projects:/projects /projects/hep/fs11/share/mnxb11/containers/mnxb11_al9.latest.sif ./calculatePI_build.sh 3e8
 fi
-if [[ "x$EXECUTE" == 'xyes' ]]; then
+
+# Launch binary execution wrapper
+if [ "x$EXECUTE" == 'xyes' ]; then
    apptainer run -B /projects:/projects /projects/hep/fs11/share/mnxb11/containers/mnxb11_al9.latest.sif ./calculatePI_run_wrapper.sh
 fi
-
-if [[ "x$DOBUILD" == 'xno' && "x$EXECUTE" == 'xno' ]]; then
-  echo "Please specify one of the options"
-  echo "  --build for building"
-  echo "  --run for execution"
-  exit
-fi
-
 
